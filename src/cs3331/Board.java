a2a117232260a58bbfa1f7adeb01c5dc8699a27e
AFTorres15 <torreand15@gmail.com>
2018-07-10 17:58:01 -0600

    Merge branches 'AndreaJuly9' and 'CesarV' of https://github.com/AFTorres15/HW3Connect5 into AndreaJuly9
    # Conflicts:
    #	src/cs3331/Board.java
    #	src/cs3331/Player.java

--- src/cs3331/Board.java
+++ src/cs3331/Board.java
@@ -1,7 +1,7 @@
     package cs3331;
 
     //import java.security.InvalidParameterException;
-
+//I am testing dumb stuff try to ckedkdkdsfjfsrajo
     /**
      * Contains the model for the Connect Five board. (No GUI elements should placed here.)
      *
@@ -79,17 +79,28 @@
         }
 
         private boolean checkForWin(Square square, Player player){
-            if(((upCheck(square.getX(),square.getY(),player.getSymbol())+downCheck(square.getX(),square.getY(),player.getSymbol())))>=6) {
+            if(((checkHelper(square.getX(),square.getY(),player.getSymbol(),0,-1)+checkHelper(square.getX(),square.getY(),player.getSymbol(),0,1)))>=6) {
                 // System.out.println("Current Points:" + currpoints);
                 return true;
             }
-            if(leftCheck(square.getX(),square.getY(),player.getSymbol())+rightCheck(square.getX(),square.getY(),player.getSymbol())>=6)
+            if(checkHelper(square.getX(),square.getY(),player.getSymbol(),-1,0)+checkHelper(square.getX(),square.getY(),player.getSymbol(),1,0)>=6)
                 return true;
-            if(leftUpCheck(square.getX(),square.getY(),player.getSymbol())+rightDownCheck(square.getX(),square.getY(),player.getSymbol())>=6)
+            if(checkHelper(square.getX(),square.getY(),player.getSymbol(),-1,-1)+checkHelper(square.getX(),square.getY(),player.getSymbol(),1,1)>=6)
                 return true;
-            return leftDownCHeck(square.getX(), square.getY(), player.getSymbol()) + rightUpCheck(square.getX(), square.getY(), player.getSymbol()) >= 6;
+            return checkHelper(square.getX(), square.getY(), player.getSymbol(),-1,1) + checkHelper(square.getX(), square.getY(), player.getSymbol(),1,-1) >= 6;
         }
 
+        private int checkHelper(int x,int y,char player,int dx,int dy){
+            try {
+                if (tiles[y][x].getPlayer().getSymbol() == player) {
+                    return 1 + checkHelper(x+dx , y+dy, player,dx,dy);
+                }
+            }catch (NullPointerException | ArrayIndexOutOfBoundsException e){
+                return 0;
+            }
+            return 0;
+        }
+        /*//upckeck dx 0 dy -1
         private int upCheck(int x, int y,char player){
             try {
                 if (tiles[y][x].getPlayer().getSymbol() == player) {
@@ -100,6 +111,7 @@
             }
             return 0;
         }
+        //dx 0,dy 1 downCheck
         private int downCheck(int x, int y, char player){
             try {
                 if(tiles[y][x].getPlayer().getSymbol()== player)
@@ -110,6 +122,7 @@
             }
             return 0;
         }
+        //left check dx -1,dy 0
         private int leftCheck(int x, int y, char player){
             try {
                 if (tiles[y][x].getPlayer().getSymbol() == player)
@@ -119,6 +132,7 @@
             }
             return 0;
         }
+        //right check dx 1 dy 0
         private int rightCheck(int x, int y, char player){
             try {
                 if (tiles[y][x].getPlayer().getSymbol() == player) {
@@ -129,6 +143,7 @@
             }
             return 0;
         }
+        //left up chekc dx -1 dy -1
         private int leftUpCheck(int x, int y, char player){
             try{
                 if(tiles[y][x].getPlayer().getSymbol()==player){
@@ -139,6 +154,7 @@
             }
             return 0;
         }
+        //right dwn check 1,1
         private int rightDownCheck(int x, int y, char player){
             try{
                 if(tiles[y][x].getPlayer().getSymbol()==player)
@@ -148,6 +164,7 @@
             }
             return 0;
         }
+        //left dwn check dx -1 dy 1
         private int leftDownCHeck(int x,int y, char player){
             try {
                 if(tiles[y][x].getPlayer().getSymbol()==player){
@@ -158,6 +175,7 @@
             }
             return 0;
         }
+        //dx1 dy -1 right up check
         private int rightUpCheck(int x, int y, char player){
             try {
                 if(tiles[y][x].getPlayer().getSymbol()== player)
@@ -166,5 +184,5 @@
                 return 0;
             }
             return 0;
-        }
+        }*/
     }
