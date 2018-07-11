4bd476189967e83a609dd2f06fc6b1732497d523
Cesar Valenzuela <kaiser122@gmail.com>
2018-07-11 14:33:04 -0600

    fixed x and y

--- src/cs3331/ConnectFive.java
+++ src/cs3331/ConnectFive.java
@@ -34,6 +34,8 @@ public class ConnectFive extends JFrame {
      */
     private BoardPanel boardPanel;
     private int squareSize = 15;
+    private Player player1 = new Player("P1", '1');
+    private Player player2 = new Player("P2", '2');
 
     /**
      * Constructor that initializes and adds all the components of the frame
@@ -101,7 +103,7 @@ public class ConnectFive extends JFrame {
                     int y = locatexy(e.getY());
                     message.setText("X: " + x + " " + "Y: " + y);
                     if (x > 0) {
-                        board.addDisc(x / 100, x % 100, new Player(1, 'o'));
+                        board.addDisc(x / 100, x % 100, new Player("Player1", '1'));
                     }
                 } catch (PlayerWonException e1) {
 
